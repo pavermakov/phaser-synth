70a904cfe734a7a2fa604a931ed3ac12436bf3a8
Pavel Ermakov <Pavel1994-09@mail.ru>
2017-07-01 22:34:57 +0300

    WEB-2. Added tap signals for synth keys

--- src/prefabs/Key.js
+++ src/prefabs/Key.js
@@ -1,21 +1,66 @@
+import Phaser from 'phaser';
 import Graphics from './base/Graphics';
 
 export default class extends Graphics {
 	constructor(game, options) {
 		super(game, options);
+
 		this._init(game, options);
 	}
 
 	_init(game, options) {
 		this.game = game;
 
-		this._draw(options);
+		this._addData(options)
+				._addSignals()
+				._addEvents()
+				._draw(options);
+
 		this.game.add.existing(this);
+	}
+
+	_addData({ id }) {
+		this.data = {
+			id,
+			signals: {},
+		};
+
+		return this;
+	}
+
+	_addSignals() {
+		this.data.signals.tapSignal = new Phaser.Signal();
+
+		return this;
+	}
+
+	_addEvents() {
+		this.events.onInputDown.add(this._handleTap, this);
+
+		return this;
 	}
 
 	_draw({ x, y, width, height, color }) {
 		this.beginFill(color);
 		this.drawRect(x, y, width, height);
 		this.endFill();
+
+		return this;
+	}
+
+	_handleTap() {
+		const { id, signals } = this.data;
+
+		signals.tapSignal.dispatch(id);
+	}
+
+	toggleInput(isEnabled) {
+		this.enabled = isEnabled;
+
+		return this;
+	}
+
+	getSignals() {
+		return this.data.signals;
 	}
 }
--- src/prefabs/base/Graphics.js
+++ src/prefabs/base/Graphics.js
@@ -3,5 +3,7 @@ import Phaser from 'phaser';
 export default class extends Phaser.Graphics {
 	constructor(game, { x, y }) {
 		super(game, x, y);
+
+		this.inputEnabled = true;
 	}
 }
