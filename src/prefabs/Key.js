e9ebfd251ed60c26d9886bd8fe9c6fd6f77726a0
Pavel Ermakov <pavermakov@gmail.com>
2017-07-09 17:59:27 +0300

    WEB-6. Added singnals between keys and play state

--- src/prefabs/Key.js
+++ src/prefabs/Key.js
@@ -5,22 +5,22 @@ export default class extends Graphics {
 	constructor(game, options) {
 		super(game, options);
 
-		this._init(game, options);
+		this.init(game, options);
 	}
 
-	_init(game, options) {
+	init(game, options) {
 		this.game = game;
 
-		this._draw(options)
-				._addData(options)
-				._addSound()
-				._addSignals()
-				._addEvents();
+		this.draw(options)
+			.initData(options)
+			.initSignals()
+			.initSound()
+			.initEvents();
 
 		this.game.add.existing(this);
 	}
 
-	_draw({ width, height, color }) {
+	draw({ width, height, color }) {
 		this.beginFill(color);
 		this.drawRect(0, 0, width, height);
 		this.endFill();
@@ -28,31 +28,37 @@ export default class extends Graphics {
 		return this;
 	}
 
-	_addData({ id, color }) {
+	initData({ id, color }) {
 		this.data = {
 			id,
 			color,
-			signals: {},
 			sound: null,
 			tweens: {},
+			signals: {
+				onKeyTapped: new Phaser.Signal(),
+			},
 		};
 
 		return this;
 	}
 
-	_addSound() {
+	initSignals() {
+		const { onKeyEnable, onKeyDisable, onKeyActive } = this.game.state.states.Play.getSignals();
+
+		onKeyEnable.add(this.toggleInput, this, 0, true);
+		onKeyDisable.add(this.toggleInput, this, 0, false);
+		onKeyActive.add(this.activate, this);
+
+		return this;
+	}
+
+	initSound() {
 		this.data.sound = this.game.add.audio(`key-${this.data.id}`, 1, false);
 
 		return this;
 	}
 
-	_addSignals() {
-		this.data.signals.tapSignal = new Phaser.Signal();
-
-		return this;
-	}
-
-	_addEvents() {
+	initEvents() {
 		this.events.onInputDown.add(this._handleInputDown, this);
 		this.events.onInputUp.add(this._handleInputUp, this);
 
@@ -61,20 +67,20 @@ export default class extends Graphics {
 
 	_handleInputDown() {
 		const { id, signals } = this.data;
+		signals.onKeyTapped.dispatch(id);
 
-		signals.tapSignal.dispatch(id);
-
-		this.activate();
+		this.activate(id);
 	}
 
 	_handleInputUp() {
 		this._changeAlpha(1);
 	}
 
-	activate(isMachine) {
+	activate(id, isMachine = false) {
+		if (this.data.id !== id) return;
+
 		this._playSound()
-				._changeAlpha(0.7)
-				._animate();
+			._changeAlpha(0.7);
 
 		if (isMachine) {
 			setTimeout(this._changeAlpha.bind(this, 1), 130);
@@ -84,10 +90,6 @@ export default class extends Graphics {
 	toggleInput(isEnabled) {
 		this.input.enabled = isEnabled;
 
-		return this;
-	}
-
-	_animate() {
 		return this;
 	}
 
