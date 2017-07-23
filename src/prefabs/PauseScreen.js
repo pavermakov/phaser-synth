b40e3d339e7d498a0e4e67ff1b09470416f21af7
Pavel Ermakov <pavermakov@gmail.com>
2017-07-23 16:41:37 +0300

    WEB-22. Updated pause screen

---
+++ src/prefabs/PauseScreen.js
@@ -0,0 +1,70 @@
+import Phaser from 'phaser';
+import Text from './base/Text';
+import Sprite from './base/Sprite';
+
+export default class extends Sprite {
+	constructor(game, options) {
+		super(game, options);
+
+		this.init(game, options);
+	}
+
+	init(game, options) {
+		this.game = game;
+
+		this.initData()
+				.initSignals(options);
+
+		this.game.add.existing(this);
+		this.initText();
+	}
+
+	initData() {
+		this.data = {
+			text: null,
+		};
+
+		return this;
+	}
+
+	initText() {
+		const options = {
+			x: this.game.world.centerX,
+			y: this.game.world.centerY,
+			text: 'PAUSED',
+			style: {
+				font: 'bold 30px Quicksand',
+				fill: 'white',
+			},
+			anchor: {
+				x: 0.5,
+				y: 0.5,
+			},
+			exists: false,
+		};
+
+		this.data.text = new Text(this.game, options);
+		this.game.add.existing(this.data.text);
+		this.data.text.exists = false;
+
+		return this;
+	}
+
+	initSignals({ uiSignals }) {
+		const { onPauseClick } = uiSignals;
+
+		onPauseClick.add(this.toggle, this);
+
+		return this;
+	}
+
+	toggle() {
+		if (this.exists) {
+			this.data.text.exists = false;
+			this.hide();
+		} else {
+			this.data.text.exists = true;
+			this.reveal();
+		}
+	}
+}
--- src/prefabs/base/Text.js
+++ src/prefabs/base/Text.js
@@ -1,10 +1,11 @@
 import Phaser from 'phaser';
 
 export default class extends Phaser.Text {
-	constructor(game, { x, y, text, style, inputEnabled, anchor, scale }) {
+	constructor(game, { x, y, text, style, inputEnabled, anchor, scale, exists }) {
 		super(game, x, y, text, style);
 
 		this.inputEnabled = inputEnabled || true;
+		this.exists = exists || true;
 
 		if (anchor) {
 			this.anchor.setTo(anchor.x, anchor.y);
